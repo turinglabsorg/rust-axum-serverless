f67e35d1064641a5e53912f9cdf156513e6e1df5
turinglabs <sebastiano.cataudo@gmail.com>
2023-11-14 08:30:31 +0100

    linting

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,6 @@
-use lambda_http::{
-    run,
-    Error,
-};
-use axum::{
-    response::Json,
-    Router,
-    routing::{get},
-};
-use serde_json::{Value, json};
+use axum::{response::Json, routing::get, Router};
+use lambda_http::{run, Error};
+use serde_json::{json, Value};
 
 async fn root() -> Json<Value> {
     Json(json!({ "msg": "🦀", "error": false }))
@@ -15,17 +8,11 @@ async fn root() -> Json<Value> {
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    // required to enable CloudWatch error logging by the runtime
     tracing_subscriber::fmt()
         .with_max_level(tracing::Level::INFO)
-        // disable printing the name of the module in every log line.
         .with_target(false)
-        // disabling time is handy because CloudWatch will add the ingestion time.
         .without_time()
         .init();
-
-    let app = Router::new()
-        .route("/", get(root));
-
+    let app = Router::new().route("/", get(root));
     run(app).await
 }
